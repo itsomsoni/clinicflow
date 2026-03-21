8b9e7de9c404cba0351b9a72bdd75fbbb00cd534
Om Soni <itsomsoni@gmail.com>
2026-03-21 16:20:15 +0530

    fix: IPatientRepository with 5 CRUD methods

--- src/PatientService/PatientService.Application/Interfaces/IPatientRepository.cs
+++ src/PatientService/PatientService.Application/Interfaces/IPatientRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PatientService.Application.DTOs;
+using PatientService.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,12 @@ using System.Threading.Tasks;
 
 namespace PatientService.Application.Interfaces
 {
-    internal interface IPatientRepository
+    public interface IPatientRepository
     {
+        Task<IEnumerable<PatientResponseDto>> GetAllAsync();
+        Task<PatientResponseDto?> GetByIdAsync(int id);
+        Task<int> CreateAsync(CreatePatientDto dto);
+        Task<bool> UpdateAsync(int id, UpdatePatientDto dto);
+        Task<bool> DeleteAsync(int id);
     }
 }
